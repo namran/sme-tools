e47e6fa54b9aaea9a99edb708ad36849a219fbb2
namran hussin <namran@imrs.com.my>
2015-11-01 01:11:24 +0800

    performed some clean up of unused comment codes.

--- app/src/main/java/com/namran/fbc/MainActivity.java
+++ app/src/main/java/com/namran/fbc/MainActivity.java
@@ -69,9 +69,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "SdOeeKTv1I7j73gjlRtEzWP7q";
     private static final String TWITTER_SECRET = "Vis0ivutpLYyuP44zFjxcSh3xB5g9tfcWoJmmKTvcM4Axt6FqA";
 
-
-
-
     private TextView info;
     private LoginButton loginButton;
 
@@ -84,8 +81,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-
-
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,12 +102,7 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new TwitterCore(authConfig), new TweetComposer());
 
 
-
-
         twloginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-
-
-
 
         final CheckBox checkBox = (CheckBox) findViewById(R.id.checkfb);
         if (checkBox.isChecked()) {
@@ -129,17 +119,9 @@ public class MainActivity extends AppCompatActivity {
         twloginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                // The TwitterSession is also available through:
-                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+
                 TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
-                //String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                //Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-                //TwitterSession session = Twitter.getSessionManager().getActiveSession();
-                TwitterAuthToken authToken = session.getAuthToken();
-                //String token = authToken.token;
-                //String secret = authToken.secret;
+
                 info.setText(
                         "User ID: "
                                 + session.getUserName()
@@ -149,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 );
 
                 // Hide login button
-                //twloginButton.setText("Logout Twitter");
+                twloginButton.setText("Logout Twitter");
                 //twloginButton.setOnClickListener(twloginButton.logout());
             }
 
@@ -168,8 +150,6 @@ public class MainActivity extends AppCompatActivity {
                 boolean handled = false;
                 if (actionId == EditorInfo.IME_ACTION_SEND) {
                     sendMessage(editText.getText().toString());
-                    //shareButton = (ShareButton)findViewById(R.id.fb_share_button);
-                    //shareButton.setShareContent();
                     handled = true;
                 }
                 return handled;
@@ -179,33 +159,28 @@ public class MainActivity extends AppCompatActivity {
         postButton.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View view) {
-                        //Log.v("EditText", EditText.getText().toString());
+                        if ( checkBox.isChecked() ) {
                         sendMessage(editText.getText().toString());
+                        }
                         if ( checkBoxtw.isChecked() ) {
                             tweetMessage(editText.getText().toString());
-                        };
+                        }
                     }
                 });
 
         clearButton.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View view) {
-                        //Log.v("EditText", EditText.getText().toString());
                         editText.setText("");
                         info.setText("");
                     }
                 });
+
         ShareLinkContent content = new ShareLinkContent.Builder()
                 .setContentUrl(Uri.parse("http://developers.facebook.com"))
                 .build();
 
-        //shareButton.setShareContent(content);
 
-        // this part is optional
-       // shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
-
-
-        // });
 
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -231,80 +206,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-/*
-        twloginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-        twloginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-            }
-
-            @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
-            }
-
-        });
-
-*/
-        /*
-        TwitterSession session = Twitter.getSessionManager().getActiveSession();
-        TwitterAuthToken authToken = session.getAuthToken();
-        String token = authToken.token;
-        String secret = authToken.secret;
-
-*/
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.namran.fbc",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
-
     }
 
     private void tweetMessage(String v) {
 
-        //AsyncTask.Status status = twitter.updateStatus(v.toString());
-        //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-
-
-        //TwitterSession session = Twitter.getSessionManager().getActiveSession();
-        //TwitterAuthToken authToken = session.getAuthToken();
-
-        /*
-        TwitterSession session = Twitter.getSessionManager().getActiveSession();
-        //TwitterAuthToken authToken = session.getAuthToken();
-
-        //final TwitterSession session = TwitterCore.getInstance().getSessionManager()
-        //        .getActiveSession();
-        final Intent intent = new ComposerActivity.Builder(this)
-            //ComposerActivity.Builder()
-               // .text("#mySME " + v.toString());
-                    .session(session)
-                .createIntent();
-        startActivity(intent);
-
-        */
-
         TweetComposer.Builder builder = new TweetComposer.Builder(this)
                       .text("#mySME "+ v.toString());
-
-        //.image();
         builder.show();
-
-
-
 
 
     }
@@ -316,22 +224,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendMessage(String v) {
-        // When Post Status Update button is clicked
-        //public void postStatusUpdate(View v){
-            // Pass null as parameter for setLink method to post status update
-           // ShareDialog shareDialog = new shareDialog.ShareLinkContentBuilder(this)
-                   // .setLink(null)
-                   // .build();
-          //  uiHelper.trackPendingDialogCall(shareDialog.present());
-       // }
-        //ShareButton shareButton = (ShareButton)findViewById(R.id.fb_share_button);
+
         ShareDialog shareDialog;
         shareDialog = new ShareDialog(this);
         if (ShareDialog.canShow(ShareLinkContent.class)) {
             ShareLinkContent linkContent = new ShareLinkContent.Builder()
                     .setContentTitle("Hello World ! Bismillah ")
-                    //.setContentDescription(
-                    //        "The 'Hello World' from simplified apps for integration.")
                     .setContentDescription(v.toString())
                     .setContentUrl(Uri.parse("http://www.namran.com"))
                     .build();
@@ -339,25 +237,6 @@ public class MainActivity extends AppCompatActivity {
             shareDialog.show(linkContent);
         }
 
-/*
-        TweetComposer.Builder builder = new TweetComposer.Builder(this)
-                .text(v.toString());
-        //.image();
-        //builder.show();
-
-        final TwitterSession session = TwitterCore.getInstance().getSessionManager()
-                .getActiveSession();
-        final Intent intent = new ComposerActivity.Builder(builder)
-                .session(session)
-                .createIntent();
-        startActivity(intent);
-
-*/
-        // The factory instance is re-useable and thread safe.
-       //Twitter twitter = TwitterFactory.getSingleton();
-        //AsyncTask.Status status = twitter.updateStatus(v.toString());
-        //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-        //info.setText(status.toString());
     }
 
     @Override
